fa140fc4b75bd7858635fe725e6d1a0f3717b97b
Delwin Perera <perera@mm.tu-darmstadt.de>
2025-06-25 21:44:17 +0200

    Solve exercise 1.11
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- sicp1.rkt
+++ sicp1.rkt
@@ -499,3 +499,31 @@ number of ways to change amount a using n kinds of coins equals:
         ((= kinds-of-coins 5) 50)))
 
 (count-change 100)
+
+#| Exercise 1.11:
+A function f is defined by the rule that
+
+f(n) = { n if n < 3
+         f(n-1) + 2f(n-2) + 3f(n-3) if n >= 3
+
+Write a procedure that computes f by means of a recursive
+process. Write a procedure that computes f by means of an
+iterative process.
+|#
+
+(define (f-recursive n)
+  (cond ((< n 3) n)
+        ((or (= n 3) (> n 3)) (+ (f-recursive (- n 1))
+                                 (* 2 (f-recursive (- n 2)))
+                                 (* 3 (f-recursive (- n 3)))))))
+
+(f-recursive 4)
+
+(define (f-iterative n)
+  (define (f-iter a b c count)
+    (if (= count n)
+        c
+        (f-iter (+ a (* 2 b) (* 3 c)) a b (+ count 1))))
+  (f-iter 2 1 0 0))
+
+(f-iterative 4)
